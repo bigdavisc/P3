3fc46bda6a495789cb793648412590b245e366aa
NoahJacobsen <noahedjacobsen@gmail.com>
2023-12-16 17:41:03 -0800

    Added simple message dividing

--- loader.py
+++ loader.py
@@ -1,5 +1,9 @@
 from flask import Flask
 import I9000
+import re
+
+MARKER = "~`~"
+MSG_RE = f"(?:^|{MARKER})(?:{MARKER})(.*)(?:{MARKER})(?:{MARKER}|$)" #Only matches one so far
 
 app = Flask(__name__)
 config = {
@@ -9,6 +13,10 @@ config = {
     "send_to_printer": True,
 }
 
+def cprint(msg):
+    if config["print_to_console"]:
+        print(msg)
+
 @app.route("/")
 def r_home():
     return f"<p>Home<br>{config}</p>"
@@ -16,9 +24,15 @@ def r_home():
 @app.route("/config/<key>/<value>")
 def r_config(key, value):
     config[key] = value
-    print(f"CONFIG: Set [{key}] to [{value}]")
+    cprint(f"CONFIG: Set [{key}] to [{value}]")
     return "<p>Updated config<p>"
 
 @app.route("/m/<msg>")
 def message(msg):
-    return f"<p>Processing command: {msg}<p>"
+    cprint(f"PARSING MESSAGE: {msg}")
+    out = ""
+    messages = re.findall(MSG_RE, msg)
+    if len(messages) == 0:
+        cprint("ERROR PARSING MESSAGE")
+        return f"<p>Error parsing message: {msg}<p>"
+    return f"<p>{messages}<p>"
