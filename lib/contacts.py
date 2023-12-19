b7071d29be4c32f33d76bf1cbeb9b903d37bd20a
NoahJacobsen <noahedjacobsen@gmail.com>
2023-12-18 19:32:36 -0800

    Added contact adding, save to file

--- lib/contacts.py
+++ lib/contacts.py
@@ -1,4 +1,3 @@
-
 class ContactsManager:
     '''
     ContactsManager
@@ -7,14 +6,19 @@ class ContactsManager:
     '''
 
     ''' Internal '''
+    # Create contacts file if it doesn't exist, otherwise load into memory
     def __init__(self):
         import os
         self.__contacts_file__ = "C://ProgramData/P3Data/contacts.txt"
         self.__err_queue__ = []
         self.__err_limit__ = 10
         self.__err_size__ = 50
+        self.__delimiter__ = "/"
+        self.__directory__ = {}  # nickname/ip/port
         if not os.path.isfile(self.__contacts_file__):
             self.__create_file__()
+        else:
+            self.__load_dir__()
     def __create_file__(self):
         import os
         import re
@@ -28,6 +32,19 @@ class ContactsManager:
             f = open(self.__contacts_file__, "x")
         except FileExistsError:
             self.__push_err__("[CREATE FILE] File already exists; cannot create")
+    def __load_dir__(self):
+        f = open(self.__contacts_file__, "r")
+        for contact in f.readlines():
+            l = contact.split(self.__delimiter__)
+            self.__directory__[l[0]] = {"ip": l[1], "port": l[2]}
+        f.close()
+    def __save_dir__(self):
+        d = []
+        for key in list(self.__directory__.keys()):
+            d.append(key+self.__delimiter__+self.__directory__[key]["ip"]+self.__delimiter__+self.__directory__[key]["port"]+"\n")
+        f = open(self.__contacts_file__, "w")
+        f.writelines(d)
+        f.close()
     def __push_err__(self, msg):
         self.__err_queue__.append(msg[:self.__err_size__])
         if len(self.__err_queue__) > self.__err_limit__:
@@ -42,7 +59,14 @@ class ContactsManager:
             #Create directories and file to satisfy
         return True # True indicates a valid file at new_path exists by func end
     def add_contact(self, nickname, ip, port):
-        return True
+        try:
+            self.__directory__[nickname]
+        except KeyError:
+            self.__directory__[nickname] = {"ip": ip, "port": port}
+            self.__save_dir__()
+            return True
+        self.__push_err__("[ADD CONTACT] Contact already exists")
+        return False
     def update_contact(self, nickname, ip, port):
         return True
     def remove_contact(self, nickname):
@@ -50,6 +74,9 @@ class ContactsManager:
 
     ''' Status funcs '''
     def list_contacts(self):
-        return []
+        return self.__directory__
     def pop_err(self):
-        return self.__err_queue__.pop(0)
+        try:
+            return self.__err_queue__.pop(0)
+        except IndexError:
+            return ""
